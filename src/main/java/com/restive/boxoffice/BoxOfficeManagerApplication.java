a1b9d24ae261fe2b1dbb87b56a3e31b358764f18
Aswathy Ayyappan <aayyappan@spireon.com>
2023-09-08 14:17:55 +0530

    Created a new repo for box office management

---
+++ src/main/java/com/restive/boxoffice/BoxOfficeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.restive.boxoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxOfficeManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoxOfficeManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/restive/boxoffice/BoxOfficeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restive.boxoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxOfficeManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
